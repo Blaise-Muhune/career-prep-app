8080640088d717da2c093e1d52ac21414ad4db82
Blaise-Muhune <71125328+Blaise-Muhune@users.noreply.github.com>
2024-11-27 03:25:21 -0500

    hope 8

--- api/config/prisma.js
+++ api/config/prisma.js
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool } from '@neondatabase/serverless';
 
-export const prisma = new PrismaClient(); 
+const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+const adapter = new PrismaNeon(neon);
+export const prisma = new PrismaClient({ adapter }); 
